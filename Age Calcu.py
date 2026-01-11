1ce0b32a00c47d22a906b908f1a5911ccb5ec841
MZDANWN <mzdawn120@gmail.com>
2026-01-11 22:03:20 +0100

    Adding colors

--- Age Calcu.py
+++ Age Calcu.py
@@ -1,18 +1,23 @@
 import datetime
 
+R = "\033[31m"
+Y = "\033[33m"
+G = "\033[32m"
 now = datetime.datetime.now()
 now_Y = now.year
 now_M = now.month
 now_D = now.day
-year = int(input("you year  my bro:"))
-mont = int(input("and mont"))
-day = int(input("and day"))
+year = int(input(f"{Y}you year  my bro: " f"{R}"))
+mont = int(input(f"{Y}and mont: " f"{R}"))
+day = int(input(f"{Y}and day: " f"{R}"))
 if now_D < day:
     now_D = now_D + 30
     now_M = now_M - 1
 if now_M < mont:
     now_M = now_M + 12
     now_Y = now_Y - 1
+if year > now_Y:
+    year = year - now_Y
 else:
     year = now_Y - year
 if mont > now_M:
