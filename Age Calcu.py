fb1616d6d4df0c64d5783771da3f4d647b02a672
Super User <root@localhost.localdomain>
2026-04-19 11:14:32 +0000

    Deep improvements

--- Age Calcu.py
+++ Age Calcu.py
@@ -1,4 +1,5 @@
 import datetime
+import calendar
 
 R = "\033[31m"
 Y = "\033[33m"
@@ -7,11 +8,27 @@ now = datetime.datetime.now()
 now_Y = now.year
 now_M = now.month
 now_D = now.day
-year = int(input(f"{Y}you year  my bro: " f"{R}"))
-mont = int(input(f"{Y}and mont: " f"{R}"))
-day = int(input(f"{Y}and day: " f"{R}"))
+
+year = int(input(f"{Y}you year: {R}"))
+if now_Y < year:
+    print("Enter a valid year")
+    exit()
+mont = int(input(f"{Y}and mont: {R}"))
+if mont < 1 or mont > 12:
+    print("month from 1 to 12")
+    exit()
+day = int(input(f"{Y}and day: {R}"))
+max_day = calendar.monthrange(year, mont)[1]
+if day < 1 or day > max_day:
+    print(f"This month there is only {max_day} days")
+    exit()
+
+if day == 29 and mont == 2:
+    if not calendar.isleap(year):
+        print("This day is not available")
+        exit()
 if now_D < day:
-    now_D = now_D + 30
+    now_D = now_D + calendar.monthrange(now_Y, now_M)[1]
     now_M = now_M - 1
 if now_M < mont:
     now_M = now_M + 12
@@ -28,4 +45,4 @@ if day > now_D:
     day = day - now_D
 else:
     day = now_D - day
-print("your age is " + str(year) + "yer/" + str(mont) + "month/" + str(day) + "day")
+print(f"{G}Your age is: {Y}{year} {G}years / {Y}{mont} {G}months / {Y}{day} {G}days")
